06792a1f97f18f5ec0ca15c1bb47889b0d096d6f
Jeremy <jeremy.fran@icloud.com>
2023-04-25 07:09:16 +0800

    feat: add page title

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { NextPage } from "next";
 import { Layout } from "@/components/layout";
-import { PageTitle } from "@/components/common";
+import {Button, PageTitle} from "@/components/common";
+import {PlusIcon} from "@heroicons/react/20/solid";
 
 const Dashboard: NextPage = () => {
   return (
     <Layout title={"Dashboard"}>
       <PageTitle title={"Dashboard"}>
-        <>Monds</>
+          <Button color={'black'}>
+              <PlusIcon className={'h-5 w-5 mr-2'}/>
+              <span>
+                  Punch In
+              </span>
+          </Button>
       </PageTitle>
     </Layout>
   );
