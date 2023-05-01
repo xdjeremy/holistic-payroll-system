fd937f7162309b0c7113a49b8c5a755c9b0ac898
jeremy <fran.jeremy@protonmail.com>
2023-05-02 01:05:33 +0800

    fix: user redirect to dashboard when already logged in

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { LoginPage } from "@/components/auth";
+import { initPocketBase } from "@/utils";
 
 const Login: NextPage = () => {
   return (
@@ -8,6 +9,30 @@ const Login: NextPage = () => {
       <LoginPage />
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const pb = await initPocketBase(ctx);
+
+    // if user is already logged in, redirect to home page
+    if (pb.authStore.isValid) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {},
+    };
+  } catch (_) {
+    return {
+      props: {},
+    };
+  }
 };
 
 export default Login;
