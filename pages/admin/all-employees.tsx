9e8885b5b34afc3a37b8960a577229b92e7aa9f6
jeremy <fran.jeremy@protonmail.com>
2023-05-07 12:58:38 +0800

    feat: check if user is admin

--- pages/admin/all-employees.tsx
+++ pages/admin/all-employees.tsx
@@ -1,6 +1,8 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { Layout } from "@/components/layout";
 import AllEmployeesPage from "@/components/admin/all-employees/all-employees.page";
+import { initPocketBase } from "@/utils";
+import { UsersPrivilegeOptions, UsersResponse } from "@/types";
 
 const AllEmployees: NextPage = () => {
   return (
@@ -10,5 +12,34 @@ const AllEmployees: NextPage = () => {
   );
 };
 
-// todo: check if user is admin
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const pb = await initPocketBase(ctx);
+  // if user is not logged in, redirect to login page
+  if (!pb.authStore.model || !pb.authStore.isValid) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const user = await pb
+    .collection("users")
+    .getOne<UsersResponse>(pb.authStore.model?.id);
+
+  // if user is not admin, 404
+  if (user.privilege !== UsersPrivilegeOptions.admin) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      user: JSON.stringify(user),
+    },
+  };
+};
+
 export default AllEmployees;
