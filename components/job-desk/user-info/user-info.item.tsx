fc4262406f12f9940f22c279be3462591d6fd71a
jeremy <fran.jeremy@protonmail.com>
2023-05-09 17:58:39 +0800

    feat: add job-desk user info data
    Signed-off-by: jeremy <fran.jeremy@protonmail.com>

--- components/job-desk/user-info/user-info.item.tsx
+++ components/job-desk/user-info/user-info.item.tsx
@@ -11,7 +11,7 @@ const UserInfoItem: FC<Props> = ({ icon, value, label }) => {
     <div className={"flex w-full flex-row items-center justify-center gap-4"}>
       <span
         className={
-          "flex h-8 w-8 flex-col items-center justify-center bg-[#F5F5F5] p-2 rounded"
+          "flex h-8 w-8 flex-col items-center justify-center rounded bg-[#F5F5F5] p-2"
         }
       >
         {icon}
@@ -19,7 +19,7 @@ const UserInfoItem: FC<Props> = ({ icon, value, label }) => {
 
       <div className={"flex w-full flex-col"}>
         <span className={"text-sm font-medium text-[#404040]"}>{value}</span>
-        <span className={"text[#9E9E9E] text-xs"}>{label}</span>
+        <span className={"text[#9E9E9E] text-xs capitalize"}>{label}</span>
       </div>
     </div>
   );
