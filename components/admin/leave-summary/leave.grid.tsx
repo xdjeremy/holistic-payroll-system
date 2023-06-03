6d7f3f5961d5646ae96802cc75ae75a248601247
jeremy <fran.jeremy@protonmail.com>
2023-06-03 18:16:19 +0800

    feat: add overview data
    Signed-off-by: jeremy <fran.jeremy@protonmail.com>

--- components/admin/leave-summary/leave.grid.tsx
+++ components/admin/leave-summary/leave.grid.tsx
@@ -1,6 +1,30 @@
 import React, { FC } from "react";
+import useSWR from "swr";
+import { pocketBase } from "@/utils";
+import { LeavesResponse, LeavesStatusOptions } from "@/types";
 
+const fetcher = async (table: string): Promise<LeavesResponse[]> => {
+  try {
+    return await pocketBase.collection(table).getFullList<LeavesResponse>();
+  } catch (err: any) {
+    throw new Error(err.data.message);
+  }
+};
 const LeaveGrid: FC = () => {
+  const { data, error } = useSWR<LeavesResponse[]>("leaves", fetcher);
+
+  const leaveTaken = data?.filter((leave) => {
+    return new Date(leave.leave_date) < new Date();
+  }).length;
+
+  const upcomingLeave = data?.filter((leave) => {
+    return new Date(leave.leave_date) > new Date();
+  }).length;
+
+  const pendingRequest = data?.filter((leave) => {
+    return leave.status === LeavesStatusOptions.pending;
+  }).length;
+
   return (
     <div
       className={
@@ -8,15 +32,15 @@ const LeaveGrid: FC = () => {
       }
     >
       <span className={"flex flex-col"}>
-        <span className={"text-5xl text-[#404040]"}>12</span>
+        <span className={"text-5xl text-[#404040]"}>{leaveTaken}</span>
         <span className={"text-md pt-2 text-[#9E9E9E]"}>Leave taken</span>
       </span>
       <span className={"flex flex-col px-4"}>
-        <span className={"text-5xl text-[#404040]"}>23 days</span>
+        <span className={"text-5xl text-[#404040]"}>{upcomingLeave}</span>
         <span className={"text-md pt-2 text-[#9E9E9E]"}>Upcoming leave</span>
       </span>
       <span className={"flex flex-col px-4"}>
-        <span className={"text-5xl text-[#404040]"}>6</span>
+        <span className={"text-5xl text-[#404040]"}>{pendingRequest}</span>
         <span className={"text-md pt-2 text-[#9E9E9E]"}>Pending Request</span>
       </span>
     </div>
